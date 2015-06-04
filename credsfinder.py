adb76ca9c3206051f01463975357793933c40b31
bandrel <justin@jbollinger.com>
2015-06-04 17:58:46 -0400

    Updated credsfinder and userquery
    Updated credsfinder to not use regex, updated userquery to use regex

--- credsfinder.py
+++ credsfinder.py
@@ -2,23 +2,22 @@
 __author__ = 'jbollin'
 import re
 
-def gen_dict_user_pass_hash(user_hash_file, hash_pass_file):
-    userhash = {}
-    hashpass = {}
-    userpasshash = {}
+"""Function to generate a dictionary that contains usernames as the keys with password and hash.
+Requires the input of file containing username:hash and a hash:crackedpasswords file"""
 
+def gen_dict_user_pass_hash(user_hash_file, hash_pass_file):
+    # initialize variables
+    userhash = {}       # Dictionary using username as the key and hash as the content.
+    hashpass = {}       # Dictionary using hash as the key and password as the content.
+    userpasshash = {}   # Dictionary using username as the key and password and hash as the content.
     with open(user_hash_file, mode="rb") as f:
         for line in f:
-            username = re.sub(':.*', "", line).rstrip()
-            phash = re.sub('.*:', "", line).rstrip()
+            username,phash = line.rstrip().split(":")
             userhash[username] = phash
-
     with open(hash_pass_file, mode="rb") as f:
         for line in f:
-            phash = re.sub(':.*', "", line).rstrip()
-            password = re.sub('.*:', "", line).rstrip()
+            phash,password = line.rstrip().split(":",1)
             hashpass[phash] = password
-
     for user in userhash.keys():
         passwordquery = hashpass[userhash[user]]
         hashquery = userhash[user]
