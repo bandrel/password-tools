2f1139c72cce43a58b7b373e251b3a041037967a
bandrel <justin.bollinger@blackbox.com>
2015-06-14 13:57:20 -0400

    Updated documentation

--- passwordquery.py
+++ passwordquery.py
@@ -1,6 +1,10 @@
 __author__ = 'jbollin'
 import sys
 import credsfinder
+"""Script that takes three CLI arguments.  NTDS output file, Hashcat output file, and a list of passwords that
+you would like to find which users have a password in the list.  The password list is line separated and case
+sensitive.  The output generated is the username/password pair since you can user multiple passwords in the password
+query file"""
 
 passlist = []
 __file__ , ntdsdump, hashcatoutput, passwordquery = sys.argv
