26ede0dd7b765cfaf0e2bb18eed49007d09e4b55
bandrel <justin@jbollinger.com>
2015-10-23 19:17:37 -0400

    added options

--- potsmash.py
+++ potsmash.py
@@ -2,19 +2,61 @@
 __author__ = 'jbollin'
 import sys
 import glob
+import getopt
 
-pots = glob.glob('*.pot')
-everything = set()
-with open(sys.argv[1],'w') as b:
-    for pot in pots:
-        with open(pot) as file:
-            for line in file:
-                a = line.split(':')
-                if len(a[0]) > 16:
-                    everything.add(line)
+def smash_pots(pots,mode,outputfile):
+    everything = set()
+    with open(outputfile,'w') as b:
+        for pot in pots:
+            with open(pot) as file:
+                for line in file:
+                    a = line.split(':')
+                    if mode == 'LM':
+                        if len(a[0]) == 16:
+                            everything.add(line)
+                    elif mode == 'NTLM':
+                        if len(a[0]) > 16:
+                            everything.add(line)
+                    else:
+                        everything.add(line)
+        for line in everything:
+            b.write(line)
+def helpmsg():
+    print 'Usage: potsmash.py [Options] ' \
+          '  -h or --help:  This help screen\n' \
+          '  -o or --output: secifies the output file name\n' \
+          '  -d or --directory: uses the directory specified.\n' \
+          '                   Defaults to current directory.\n' \
+          '  -n or --ntlm: specifies output to only be ntlm\n' \
+          '  -l or --lm: specifies output to only be ntlm\n'
 
-    for line in everything:
-        b.write(line)
+if __name__ == '__main__':
+    mode = 'ALL'
+    pots = glob.glob('*.pot')
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hd:lno:',['help', 'directory=', 'lm', 'ntlm', 'output='])
+    except getopt.GetoptError as err:
+        helpmsg()
+        print str(err)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            helpmsg()
+            sys.exit()
+        elif opt in ('-d', '--directory'):
+            working_dir = arg
+        elif opt in ('-l', '--lm'):
+            mode = 'LM'
+        elif opt in ('-n', '--ntlm'):
+            mode = 'NTLM'
+        elif opt in ('-o', '--output'):
+            outputfile = arg
+    if outputfile == '':
+        print '[!] you must specify an output file name'
+        sys.exit(2)
+    smash_pots(pots,mode,outputfile)
 
 
 
