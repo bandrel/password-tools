cd65a539d272ec43c3dfb68e0c16cb5ba68b34cf
bandrel <justin@jbollinger.com>
2015-10-23 12:30:51 -0400

    second commit

--- dicsmash.py
+++ dicsmash.py
@@ -10,36 +10,34 @@ filetype = glob.glob(str('*.'+sys.argv[1]))
 outputfile = sys.argv[3]
 tempfiles = []
 
-def split_to_size(file,size):
+def split_to_size(file):
     global tempfiles
-    tempfile = str(str(size)+'.dictemp')
-    tempfiles.append(tempfile)
-    with open(tempfile,'a') as f:
-        current_working_list = []
-        with open(file) as origionaldic:
-            for line in origionaldic:
-                if len(line.strip('\r\n')) == size:
-                    current_working_list.append(line.strip('\r\n'))
-        for line in current_working_list:
-            f.write(line+'\n')
+    with open(file) as origionaldic:
+        for line in origionaldic:
+            current_length = len(line.rstrip())
+            tempfile = str(str(current_length)+'.dictemp')
+            tempfiles.append(tempfile)
+            with open(tempfile, 'a') as a:
+                 a.write(line)
 
 def dedupe_and_merge(outname):
+    working_list = []
     with open(outname,'w') as outfile:
         for file in tempfiles:
-            working_set = set()
             with open(file) as current_working_file:
                 for line in current_working_file:
-                    working_set.add(line)
-            for line in working_set:
-                outfile.write(line)
+                    working_list.append(line)
+        working_set = set(working_list)
+        for line in working_set:
+            outfile.write(line)
 
 def cleanup_temp():
+    tempfiles = glob.glob('*.dictemp')
     for tfile in tempfiles:
         os.remove(tfile)
 
 if __name__ == '__main__':
     for file in filetype:
-        for size in xrange(1,maxlength):
-            split_to_size(file,size)
+        split_to_size(file)
     dedupe_and_merge(outputfile)
     cleanup_temp()
