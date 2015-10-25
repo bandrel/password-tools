3c35c40abf547653661b1697019d1f9d7684d757
bandrel <justin@jbollinger.com>
2015-10-25 15:25:41 -0400

    Added verbose output option and created timestamps for verbose output

--- dicsmash.py
+++ dicsmash.py
@@ -4,10 +4,11 @@ import sys
 import glob
 import os
 import getopt
-
+import datetime
 
 def split_to_size(file):    #read file line by line and copy the password into a file that corresponds to the length
-    print '[+] Splitting %s' % file
+    if verbose_mode == True:
+        print '[+] Splitting %s' % file
     with open(file) as origionaldic:
         for line in origionaldic:
             current_length = len(line.rstrip())
@@ -24,7 +25,9 @@ def dedupe_and_merge(outname):  #reads each tempfile in and then adds the conten
     tempfiles.sort()
     with open(outname,'w') as outfile:
         for file in tempfiles:
-            print '[+] Processing %s' % file
+            if verbose_mode == True:
+                print '[*] Processing %s' % file
+                print_elapsed()
             working_set = set()
             with open(file) as current_working_file:
                 for line in current_working_file:
@@ -34,9 +37,11 @@ def dedupe_and_merge(outname):  #reads each tempfile in and then adds the conten
             gc.collect()
 
 def cleanup_temp():         #delets the dictemp files
-   print '[+] Cleaning up tempfiles'
-   for tfile in tempfiles:
+    if verbose_mode == True:
+        print '[+] Cleaning up tempfiles'
+    for tfile in tempfiles:
         os.remove(tfile)
+
 
 def helpmsg():
     print 'Usage: dicsmash.py [Options] ' \
@@ -44,16 +49,23 @@ def helpmsg():
           '  -d or --directory: uses the directory specified.\n' \
           '                   Defaults to current directory.\n' \
           '  -e or --extension: specifies the extension of the input dictionaries\n' \
-          '  -o or --output: Specifies the output file name of the new dictionary\n'
+          '  -o or --output: Specifies the output file name of the new dictionary\n' \
+          '  -v or --verbose: Creates verbose output\n'
+def print_elapsed():
+    if verbose_mode == True:
+        print '[+] %s since start' % elapsed_time
 tempfiles = []
 directory = os.curdir
 extension = 'dic'
 working_dir = os.curdir
+verbose_mode = False
+start_time = datetime.datetime.now()
+elapsed_time = datetime.datetime.now() - start_time
 if __name__ == '__main__':
     #Program defaults
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hd:e:o:',['help', 'directory=', 'extension=', 'output='])
+        opts, args = getopt.getopt(sys.argv[1:], 'hd:e:o:v',['help', 'directory=', 'extension=', 'output=','verbose'])
     except getopt.GetoptError as err:
         helpmsg()
         print str(err)
@@ -69,6 +81,9 @@ if __name__ == '__main__':
             extension = arg
         elif opt in ('-o', '--output'):
             outputfile = arg
+        elif opt in ('-v', '--verbose'):
+            verbose_mode = True
+
 
     try:
         os.chdir(working_dir)
@@ -82,8 +97,13 @@ if __name__ == '__main__':
         sys.exit(2)
 
 
-
     for file in filetype:
         split_to_size(file)
+        print_elapsed()
     dedupe_and_merge(outputfile)
     cleanup_temp()
+    elapsed_time = datetime.datetime.now() - start_time
+    print_elapsed()
+    finished_time = datetime.datetime.now()
+    print '[*] Finished after %s seconds' % elapsed_time
+
